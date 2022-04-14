1d836827d45cd65d125900d1a83f15c54078fe05
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-14 16:04:53 -0300

    Update gitignore, pom and configs

--- src/main/java/br/com/zup/edu/nossozenity/NossoZenityApplication.java
+++ src/main/java/br/com/zup/edu/nossozenity/NossoZenityApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NossoZenityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NossoZenityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NossoZenityApplication.class, args);
+    }
 
 }
